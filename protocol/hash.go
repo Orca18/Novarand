6d3d1635c6b98ee1eb267e9f11a38859dabc9464
orca18 <lukedev1008@gmail.com>
2022-04-07 13:06:29 +0900

    Algrand to Novarand

--- protocol/hash.go
+++ protocol/hash.go
@@ -18,10 +18,14 @@ package protocol
 
 // HashID is a domain separation prefix for an object type that might be hashed
 // This ensures, for example, the hash of a transaction will never collide with the hash of a vote
+// HashID는 해시될 수 있는 객체 유형에 대한 도메인 분리 접두사입니다.
+// 이것은 예를 들어 트랜잭션의 해시가 투표의 해시와 충돌하지 않도록 합니다.
 type HashID string
 
 // Hash IDs for specific object types, in lexicographic order.
 // Hash IDs must be PREFIX-FREE (no hash ID is a prefix of another).
+// 사전순으로 특정 객체 유형에 대한 해시 ID.
+// 해시 ID는 PREFIX-FREE여야 합니다(해시 ID가 다른 해시 ID의 접두어가 아님).
 const (
 	AppIndex HashID = "appID"
 
@@ -31,7 +35,7 @@ const (
 	// "arcXXXX" (where "XXXX" is the 0-padded number of the ARC)
 	// For example ARC-0003 can use any prefix starting with "arc0003"
 	// ===================================================================
-	// ARCReserved는 `arc`로 시작하는 접두사를 예약하는 데 사용됩니다.
+	// ARCReserved는 'arc' 로 시작하는 접두사를 ARC 관련  해시에 예약하는 데 사용
 	// ARC 관련 해시 https://github.com/algorandfoundation/ARCs
 	// ARC-XXXX의 접두사는 다음으로 시작해야 합니다.
 	// "arcXXXX"(여기서 "XXXX"는 ARC의 0으로 채워진 숫자)
